1c503ba4344cc6ffc1899ab8db156f8b1a469dcf
Christopher <calb200@aucklanduni.ac.nz>
2021-02-14 09:55:39 +1300

    setup portfolio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import { Route } from 'react-router-dom';
-
+import React, { Fragment } from 'react';
 import Homepage from './Pages/Homepage';
-
+import Portfolio from './Pages/Portfolio';
 const App = () => {
-  return <Route exact path="/" component={Homepage} />;
+  return (
+    <Fragment>
+      <Route exact path="/christopheralba" component={Homepage} />
+      <Route exact path="/christopheralba/portfolio" component={Portfolio} />
+    </Fragment>
+  );
 };
 
 export default App;
---
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Portfolio = () => {
+  return <div>Welcome to Portfolio</div>;
+};
+
+export default Portfolio;
