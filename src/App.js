071686c847ab5c60906ad423d8ec42a1ae9e0893
Christopher <calb200@aucklanduni.ac.nz>
2021-02-14 21:49:46 +1300

    complete navbar styling and display

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import { Route } from 'react-router-dom';
 import React, { Fragment } from 'react';
 import Homepage from './Pages/Homepage';
 import Portfolio from './Pages/Portfolio';
+import NavbarMain from './Components/Navbar';
 const App = () => {
   return (
     <Fragment>
+      <Route
+        path="/"
+        render={(props) => <NavbarMain pathName={props.location.pathname} />}
+      />
       <Route exact path="/" component={Homepage} />
       <Route exact path="/portfolio" component={Portfolio} />
     </Fragment>
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import './navbar.css';
+
+const NavbarMain = (props) => {
+  const [location, setLocation] = useState(props.pathName);
+
+  useEffect(() => {
+    setLocation(props.pathName);
+  }, [props.pathName]);
+
+  return (
+    <Navbar className="navbar-main" bg="dark" expand="lg" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Christopher Sy Alba</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link className={`${location === '/' ? 'active' : ''} nav-link-home`} href="/#/">
+              Home
+            </Nav.Link>
+            <Nav.Link
+              className={location === '/portfolio' ? 'active' : ''}
+              href="/#/portfolio"
+            >
+              Portfolio
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavbarMain;
