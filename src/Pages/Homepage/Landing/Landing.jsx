a34f213a275188a6ec717973595082e48448cf48
Christopher <calb200@aucklanduni.ac.nz>
2021-02-20 11:55:44 +1300

    finish styling landing area in homepage

--- src/Pages/Homepage/Landing/Landing.jsx
+++ src/Pages/Homepage/Landing/Landing.jsx
@@ -1,22 +1,39 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import Button from "react-bootstrap/Button";
 
-import './landing.css';
+import "./landing.css";
 const Landing = () => {
   return (
     <div className="landing-hero-image">
       <div className="landing-hero-text">
         <div>
           <h1>Christopher Sy Alba</h1>
-          <ul>
-            <li>Web Developer</li>
-            <li>Computer Systems Engineering Student</li>
-          </ul>
+          <h5>
+            <em> Web Developer</em>
+          </h5>
+          <h5 className="sub-header-compsys">
+            <em>Computer Systems Engineering Student</em>
+          </h5>
           <div>
-            <Button className="landing-button-light" variant="light" href="https://docs.google.com/document/d/16pRyFT4RnE5ICkokCWPxguS-Z14z-i5bxwUf2Sz1doE/edit?usp=sharing"  target="_blank">VIEW MY CV</Button>
-            <Button className="landing-button-light" variant="light" href="christopheralba/#/portfolio">VIEW MY PORTFOLIO</Button>
+            <Button
+              className="landing-button-light"
+              variant="light"
+              href="https://docs.google.com/document/d/16pRyFT4RnE5ICkokCWPxguS-Z14z-i5bxwUf2Sz1doE/edit?usp=sharing"
+              target="_blank"
+            >
+              VIEW MY CV
+            </Button>
+            <Button
+              className="landing-button-light"
+              variant="light"
+              href="christopheralba/#/portfolio"
+            >
+              VIEW MY PORTFOLIO
+            </Button>
           </div>
-          <Button className="landing-button-info" variant="info">LEARN MORE ABOUT ME</Button>
+          <Button className="landing-button-info">
+            LEARN MORE ABOUT ME
+          </Button>
         </div>
       </div>
     </div>
