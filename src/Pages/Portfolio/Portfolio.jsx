6b6494b292c8c179cf3b20a99d33fd3e6f0900b6
Christopher <calb200@aucklanduni.ac.nz>
2021-02-25 22:11:01 +1300

    add portfolio basic display

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
-
+import { Button } from "react-bootstrap";
+import React from "react";
+import { Container } from "react-bootstrap";
+import projects from "./projects";
+import "./portfolio.css"
 const Portfolio = () => {
-  return <div>Welcome to Portfolio</div>;
+  return (
+    <Container>
+      <div className="portfolio-div">
+        {projects.map((project) => (
+          <div className="portfolio-project-div">
+            <img src={project.imgUrl} alt="" />
+            <Button className="visit" href={project.href} variant="dark">
+              Visit
+            </Button>
+            <h4>{project.title}</h4>
+            <p>{project.description}</p>
+            <div className="hashtags">
+              {project.tags.map((tag) => (
+                <div className="hashtag">
+                  <strong>#{tag}</strong>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default Portfolio;
