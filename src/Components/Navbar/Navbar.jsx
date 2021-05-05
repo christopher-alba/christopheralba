4a726fc2715f02aabd8c818aee2699d0def33ebc
Christopher <calb200@aucklanduni.ac.nz>
2021-05-05 19:41:29 +1200

    get rid of dynamic feature of navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,22 +6,6 @@ import "./navbar.css";
 
 const NavbarMain = (props) => {
   const [location, setLocation] = useState(props.pathName);
-  const [navbarToggle, setNavbarToggle] = useState(true);
-
-  var prevScrollpos = window.pageYOffset;
-  window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-      document.getElementsByClassName("navbar-main")[0].style.top = "0";
-      document.getElementsByClassName("navbar-main")[0].style.opacity = "1";
-      setNavbarToggle(true);
-    } else {
-      document.getElementsByClassName("navbar-main")[0].style.top = "-100px";
-      document.getElementsByClassName("navbar-main")[0].style.opacity = "0";
-      setNavbarToggle(false);
-    }
-    prevScrollpos = currentScrollPos;
-  };
 
   useEffect(() => {
     setLocation(props.pathName);
@@ -29,24 +13,12 @@ const NavbarMain = (props) => {
 
   return (
     <Fragment>
-      {!navbarToggle && (
-        <button className="navbar-down" aria-label="show navbar" onClick={() => setNavbarToggle(true)} >
-          <i className="fas fa-chevron-down"></i>
-        </button>
-      )}
       <Navbar
         className="navbar-main"
-        style={{
-          top: navbarToggle ? "0" : "-70px",
-          opacity: navbarToggle ? 1 : 0,
-        }}
         bg="dark"
         expand="lg"
         variant="dark"
       >
-        <button className="navbar-up" aria-label="hide navbar" onClick={() => setNavbarToggle(false)}>
-          <i className="fas fa-times"></i>
-        </button>
         <Container>
           <Navbar.Brand href="#/">Christopher Sy Alba</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
